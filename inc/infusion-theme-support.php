b5f5fae97fc3aa7076050a88968dd97b332f2132
Tarei King <tareiking@gmail.com>
2014-06-25 22:12:22 +1000

    Added theme-support foundation

---
+++ inc/infusion-theme-support.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Add Support for various scripts used in Infusion
+ *
+ * @subpackage built on ./foundation-theme-support.php
+ */
+
+function infusion_check_theme_support() {
+}
+
+add_action( 'init', 'infusion_check_theme_support' );
