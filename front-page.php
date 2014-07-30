0fc7e4ab7fac3ae687c0d0b5c1eda7a90023b9c5
Tarei King <tareiking@gmail.com>
2014-07-30 21:54:09 +1000

    Frontpage loads only the content. No titles or meta :)

--- front-page.php
+++ front-page.php
@@ -39,14 +39,15 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'parts/content', 'single' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) {
-						comments_template();
-					}
-				?>
+				<div class="entry-content">
+					<?php the_content(); ?>
+					<?php
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . __( 'Pages:', 'flair' ),
+							'after'  => '</div>',
+						) );
+					?>
+				</div><!-- .entry-content -->
 
 			<?php endwhile; // end of the loop. ?>
 
