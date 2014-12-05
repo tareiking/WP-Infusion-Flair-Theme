baa0956626c1d5b64b9a6d2b8fe239ec44d54b45
Tarei King <tareiking@gmail.com>
2014-12-05 19:01:57 +1000

    Fix #12. Re-run foundation equalizer if we've got flexslider
    This doesn't quite fix the potential enqueuing problem with pre-requisites loading before app.js,
    but its friday #yolo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,17 @@
 
 (function($) {
-	$(document).foundation(
-		'topbar', {
-			mobile_show_parent_link: true
-		}
-	);
+	$(document).foundation();
 	var doc = document.documentElement;
 	doc.setAttribute('data-useragent', navigator.userAgent);
+
+	// Flexslider loads after foundation init's, so lets reshuffle
+	if ( $( '.flexslider' ).length > 0 ) {
+		$(window).load(function(){
+			$(document).foundation({
+				equalizer : {
+					equalize_on_stack: true
+				}
+			});
+		});
+	};
 })(jQuery);
